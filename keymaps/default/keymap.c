3d4edef06d614df192bf6e1fb871ae07ab65c4a3
justNeto <continental.lelgg.2b@gmail.com>
2024-11-22 12:37:39 -0600

    Testing the newly repaired letter

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -36,22 +36,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
     [0] = LAYOUT(
-            TO(0),              KC_1,           KC_2,                   KC_3,                   KC_4,                   KC_5,       KC_PRINT_SCREEN,                TO(1),                      KC_6,           KC_7,               KC_8,               KC_9,           KC_0,       KC_NO,
+            TO(0),              KC_1,           KC_2,                   KC_3,                   KC_4,                   KC_5,       KC_PRINT_SCREEN,                KC_NO,                      KC_6,           KC_7,               KC_8,               KC_9,           KC_0,       TG(2),
             TMUX_MACRO,         KC_SCLN,        KC_COMMA,               KC_DOT,                 KC_P,                   KC_Y,       KC_AUDIO_VOL_UP,                KC_AUDIO_VOL_DOWN,          KC_F,           KC_G,               KC_C,               KC_R,           KC_L,       KC_TILDE,
-            TG(3),              KC_A,           KC_O,                   KC_E,                   KC_U,                   KC_I,       KC_QUESTION,                    KC_EXCLAIM,                 KC_D,           KC_H,               KC_T,               KC_N,           KC_S,       CW_TOGG,
-            KC_LEFT_SHIFT,      KC_QUOTE,       KC_Q,                   KC_J,                   KC_K,                   KC_X,       KC_NO,                          KC_NO,                      KC_B,           KC_M,               KC_W,               KC_V,           KC_Z,       KC_RIGHT_SHIFT,
-                                                LGUI_T(KC_ESCAPE),      LT(2, KC_SPACE),        LCTL_T(KC_TAB),                                                                                                 KC_LEFT_ALT,        KC_BACKSPACE,       KC_ENT
+            TG(4),              KC_A,           KC_O,                   KC_E,                   KC_U,                   KC_I,       KC_QUESTION,                    KC_EXCLAIM,                 KC_D,           KC_H,               KC_T,               KC_N,           KC_S,       CW_TOGG,
+            KC_LEFT_SHIFT,      KC_QUOTE,       KC_Q,                   KC_J,                   KC_K,                   KC_X,       TG(1),                          TG(2),                      KC_B,           KC_M,               KC_W,               KC_V,           KC_Z,       KC_RIGHT_SHIFT,
+                                                LGUI_T(KC_ESCAPE),      LT(3, KC_SPACE),        LCTL_T(KC_TAB),                                                                                                 KC_LEFT_ALT,        KC_BACKSPACE,       KC_ENT
     ),
 
     /*
-     * LoL gaming layer
+     * LoL gaming
      */
     [1] = LAYOUT(
             KC_TRNS,            KC_1,           KC_2,                   KC_3,                   KC_4,               KC_5,       KC_P,                       KC_NO,        KC_TRNS,      KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,
             KC_TAB,             KC_Q,           KC_W,                   KC_E,                   KC_R,               KC_T,       KC_NO,                      KC_NO,        KC_Y,         KC_U,           KC_I,           KC_O,           KC_P,           KC_TRNS,
             KC_ESCAPE,          KC_A,           KC_S,                   KC_D,                   KC_F,               KC_G,       KC_NO,                      KC_NO,        KC_G,         KC_H,           KC_J,           KC_K,           KC_L,           KC_TRNS,
             KC_TRNS,            KC_Z,           KC_X,                   KC_C,                   KC_V,               KC_B,       KC_M,                       KC_NO,        KC_N,         KC_M,           KC_COMMA,       KC_DOT,         KC_SCLN,        KC_TRNS,
-                                                LALT_T(KC_A),           KC_TRNS,                KC_LEFT_CTRL,                                                                           KC_TRNS,        KC_TRNS,        KC_TRNS
+                                                LALT_T(KC_A),           LT(4, KC_SPACE),        KC_LEFT_CTRL,                                                                           KC_TRNS,        KC_TRNS,        KC_TRNS
+    ),
+
+    /*
+     * General gaming
+     */
+    [2] = LAYOUT(
+            KC_TRNS,            KC_1,           KC_2,                   KC_3,                   KC_4,               KC_5,       KC_P,                       KC_NO,        KC_TRNS,      KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,
+            KC_TAB,             KC_Q,           KC_W,                   KC_E,                   KC_R,               KC_T,       KC_NO,                      KC_NO,        KC_Y,         KC_U,           KC_I,           KC_O,           KC_P,           KC_TRNS,
+            KC_ESCAPE,          KC_A,           KC_S,                   KC_D,                   KC_F,               KC_G,       KC_NO,                      KC_NO,        KC_G,         KC_H,           KC_J,           KC_K,           KC_L,           KC_TRNS,
+            KC_TRNS,            KC_Z,           KC_X,                   KC_C,                   KC_V,               KC_B,       KC_M,                       KC_NO,        KC_N,         KC_M,           KC_COMMA,       KC_DOT,         KC_SCLN,        KC_TRNS,
+                                                LALT_T(KC_R),           KC_SPACE,               KC_LEFT_CTRL,                                                                           KC_TRNS,        KC_TRNS,        KC_TRNS
     ),
 
     /*
@@ -79,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *
      */
 
-    [2] = LAYOUT(
+    [3] = LAYOUT(
             KC_TRNS,            KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,            KC_TRNS,                    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,      KC_NO,
             KC_NO,              KC_COMMA,       KC_GRAVE,       KC_AMPR,        KC_LBRC,        KC_LABK,            KC_NO,                      KC_NO,          KC_RABK,        KC_PIPE,        KC_BSLS,        KC_NO,          KC_NO,      KC_NO,
             KC_0,               KC_COLN,        KC_QUOTE,       KC_LPRN,        KC_LCBR,        KC_PLUS,            KC_NO,                      KC_NO,          KC_EQUAL,       KC_RCBR,        KC_RPRN,        KC_SLASH,       KC_SCLN,    KC_DLR,
@@ -92,12 +103,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * Number + F keys + arrows layer
      *
      */
-    [3] = LAYOUT(
+    [4] = LAYOUT(
             KC_TRANSPARENT,     KC_NO,      KC_NO,              KC_NO,                  KC_NO,              KC_NO,      KC_NO,                                      KC_NO,          KC_NO,          KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
             KC_NO,              KC_F1,      KC_F2,              KC_F3,                  KC_F4,              KC_F5,      KC_NO,                                      KC_NO,          KC_F6,          KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_NO,
             KC_TRANSPARENT,     KC_1,       KC_2,               KC_3,                   KC_4,               KC_5,       KC_NO,                                      KC_NO,          KC_6,           KC_7,       KC_8,       KC_9,       KC_0,       KC_NO,
             KC_LEFT_SHIFT,      KC_F11,     KC_NO,              KC_LEFT,                KC_DOWN,            KC_NO,     KC_NO,                                       KC_NO,          KC_NO,          KC_UP,      KC_RIGHT,   KC_F12,     KC_NO,      KC_RIGHT_SHIFT,
-                                            KC_TRANSPARENT,     KC_TRANSPARENT,         KC_TRANSPARENT,                                                                                             KC_NO,      KC_NO,      KC_NO
+                                            KC_TRANSPARENT,     KC_TRANSPARENT,         KC_TRANSPARENT,                                                                                             KC_TRNS,    KC_TRNS,    KC_TRNS
     )
 };
 
